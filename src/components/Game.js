233b5ac322dc6b86f3488be85b4f86b6dbfd1a86
MamadTvl <mtavallai80@gmail.com>
2021-06-09 21:55:28 +0430

    add scroll

--- src/components/Game.js
+++ src/components/Game.js
@@ -1,5 +1,5 @@
 import React from "react";
-import {Text, View} from "react-native";
+import {ScrollView, Text, View} from "react-native";
 import {Picker} from '@react-native-picker/picker';
 import {Button} from 'react-native-paper';
 import {styles} from "./styles/GameStyle";
@@ -25,96 +25,98 @@ const Game = () => {
     const dispatch = useDispatch()
 
     return (
-        <View style={styles.container}>
-            <Text style={styles.turn}>
-                {turn === 1 && player1.name}
-                {turn === 2 && player2.name}'s turn
-            </Text>
-            <View style={styles.pickerContainer}>
-                <Picker
-                    style={styles.picker}
-                    mode={'dialog'}
-                    selectedValue={difficulty}
-                    onValueChange={
-                        (itemValue) => dispatch(changeDifficulty(itemValue))
-                    }
-                >
-                    <Picker.Item label={'easy'} value={'easy'}/>
-                    <Picker.Item label={'hard'} value={'hard'}/>
-                    <Picker.Item label={'impossible'} value={'impossible'}/>
-                </Picker>
-                <Picker
-                    style={styles.picker}
-                    mode={'dialog'}
-                    selectedValue={mode}
-                    onValueChange={
-                        (itemValue) => dispatch(changeMode(itemValue))
-                    }
-                >
-                    <Picker.Item label={'robot'} value={'robot'}/>
-                    <Picker.Item label={'friendly'} value={'friendly'}/>
-                </Picker>
-            </View>
-            <Board/>
-            {
-                player1.winner && player2.winner ?
-                    <Text style={styles.info}>Draw</Text>
-                    :
-                    player1.winner ?
-                        <Text style={styles.info}>
-                            {player1.winner && `${player1.name} win`}
-                        </Text>
-                        :
-                        <Text style={styles.info}>
-                            {player2.winner && `${player2.name} win`}
-                        </Text>
-            }
-            <Button
-                mode={'contained'}
-                onPress={() => dispatch(restartGame())}
-
-            >
-                Restart
-            </Button>
-            <CodeIcon/>
-            <View style={{
-                display: 'flex',
-                flexDirection: 'row',
-            }}>
-                <Text
-                    style={{
-                        fontSize: 16,
-                        marginRight: 8,
-                    }}
-
-                >
-                    created with
+        <ScrollView>
+            <View style={styles.container}>
+                <Text style={styles.turn}>
+                    {turn === 1 && player1.name}
+                    {turn === 2 && player2.name}'s turn
                 </Text>
-                <HeartIcon/>
-                <Text
-                    style={{
+                <View style={styles.pickerContainer}>
+                    <Picker
+                        style={styles.picker}
+                        mode={'dialog'}
+                        selectedValue={difficulty}
+                        onValueChange={
+                            (itemValue) => dispatch(changeDifficulty(itemValue))
+                        }
+                    >
+                        <Picker.Item label={'easy'} value={'easy'}/>
+                        <Picker.Item label={'hard'} value={'hard'}/>
+                        <Picker.Item label={'impossible'} value={'impossible'}/>
+                    </Picker>
+                    <Picker
+                        style={styles.picker}
+                        mode={'dialog'}
+                        selectedValue={mode}
+                        onValueChange={
+                            (itemValue) => dispatch(changeMode(itemValue))
+                        }
+                    >
+                        <Picker.Item label={'robot'} value={'robot'}/>
+                        <Picker.Item label={'friendly'} value={'friendly'}/>
+                    </Picker>
+                </View>
+                <Board/>
+                {
+                    player1.winner && player2.winner ?
+                        <Text style={styles.info}>Draw</Text>
+                        :
+                        player1.winner ?
+                            <Text style={styles.info}>
+                                {player1.winner && `${player1.name} win`}
+                            </Text>
+                            :
+                            <Text style={styles.info}>
+                                {player2.winner && `${player2.name} win`}
+                            </Text>
+                }
+                <Button
+                    mode={'contained'}
+                    onPress={() => dispatch(restartGame())}
+
+                >
+                    Restart
+                </Button>
+                <CodeIcon/>
+                <View style={{
+                    display: 'flex',
+                    flexDirection: 'row',
+                }}>
+                    <Text
+                        style={{
+                            fontSize: 16,
+                            marginRight: 8,
+                        }}
+
+                    >
+                        created with
+                    </Text>
+                    <HeartIcon/>
+                    <Text
+                        style={{
+                            fontSize: 16,
+                            marginLeft: 8,
+                        }}
+                    >
+                        By
+                    </Text>
+                    <Text style={{
                         fontSize: 16,
                         marginLeft: 8,
+                        textDecorationLine: "underline",
+                        textDecorationStyle: "solid",
+                        textDecorationColor: "#607D8B"
                     }}
-                >
-                    By
-                </Text>
-                <Text style={{
-                    fontSize: 16,
-                    marginLeft: 8,
-                    textDecorationLine: "underline",
-                    textDecorationStyle: "solid",
-                    textDecorationColor: "#607D8B"
-                }}
-                      onPress={() => {
-                          Linking.openURL('https://github.com/MamadTvl/TicTacToe-React-Native')
-                      }}
-                >
-                    MamadTvl
-                </Text>
+                          onPress={() => {
+                              Linking.openURL('https://github.com/MamadTvl/TicTacToe-React-Native')
+                          }}
+                    >
+                        MamadTvl
+                    </Text>
 
+                </View>
             </View>
-        </View>
+        </ScrollView>
     )
 }
 
